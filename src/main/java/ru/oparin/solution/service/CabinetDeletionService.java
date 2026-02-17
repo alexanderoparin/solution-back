b36ee931c41149acf03c727c57036e043ef5e6fd
alex_oparin <alexandroparin09@gmail.com>
2026-02-17 18:34:20 +0300

    увеличил батч при каскадном удалении пользака

--- src/main/java/ru/oparin/solution/service/CabinetDeletionService.java
+++ src/main/java/ru/oparin/solution/service/CabinetDeletionService.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Slf4j
 public class CabinetDeletionService {
 
-    private static final int BATCH_SIZE = 20;
+    private static final int BATCH_SIZE = 50;
 
     private final PromotionCampaignStatisticsRepository promotionCampaignStatisticsRepository;
     private final CampaignArticleRepository campaignArticleRepository;
